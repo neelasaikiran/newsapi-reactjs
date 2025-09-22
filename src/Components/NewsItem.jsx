ff7d5f39cf3e5b98796582bceffbc10a16e17646
NEELA SAIKIRAN <neelasaikiran123@gmail.com>
2025-09-22 17:20:17 +0530

    api updated

---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,36 @@
+
+import image from '../assets/news.png'
+
+const NewsItem = ({ title, description, src, url }) => {
+  return (
+    <div
+      className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2"
+      style={{ maxWidth: "345px" }}
+    >
+      <img
+        src={src ? src : image}
+        className="card-img-top"
+        style={{ height: "200px", objectFit: "cover" }}
+        alt={title || "News thumbnail"}
+      />
+      <div className="card-body">
+        <h5 className="card-title">
+          {title ? title.slice(0,50) : "Untitled"}
+        </h5>
+        <p className="card-text">
+          {description ? description.slice(0, 80) : "No description available."}
+        </p>
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-primary"
+        >
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
